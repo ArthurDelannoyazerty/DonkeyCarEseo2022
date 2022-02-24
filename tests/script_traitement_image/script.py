6e60d0318a3b3b289d19d19007f75fe4e327416b
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2022-02-24 09:30:23 +0100

    Merge branch 'opencv_nonteste' of https://github.com/ArthurDelannoyazerty/DonkeyCarEseo2022 into opencv_nonteste

--- tests/script_traitement_image/script.py
+++ tests/script_traitement_image/script.py
@@ -1,11 +1,35 @@
+import argparse
 import cv2 as cv
 import sys
+from cv2 import threshold
 import numpy as np
 
+"""
+max_value = 255
+max_type = 4
+max_binary_value = 255
+trackbar_type = 'Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted'
+trackbar_value = 'Value'
+window_name = 'Threshold Demo'
 
+def Threshold_Demo(val):
+    #0: Binary
+    #1: Binary Inverted
+    #2: Threshold Truncated
+    #3: Threshold to Zero
+    #4: Threshold to Zero Inverted
+    threshold_type = cv.getTrackbarPos(trackbar_type, window_name)
+    threshold_value = cv.getTrackbarPos(trackbar_value, window_name)
+    _, dst = cv.threshold(gray, threshold_value, max_binary_value, threshold_type )
+    cv.imshow(window_name, dst)
+
+parser = argparse.ArgumentParser(description='Code for Basic Thresholding Operations tutorial.')
+parser.add_argument('--input', help='Path to input image.', default='867_cam_image_array_.jpg')
+args = parser.parse_args()
+"""
 
 ##load imagesv
-origin = cv.imread(cv.samples.findFile("original_2k22.jpg"))
+origin = cv.imread(cv.samples.findFile("867_cam_image_array_.jpg"))
 if origin is None:
     sys.exit("Could not read the image.")
 
@@ -47,8 +71,20 @@ t = (e2 - e1)/cv.getTickFrequency()
 print( "gray  : " ,t)
 
 
+#threshold
+e1 = cv.getTickCount()
+
+_,thresholdImage = cv.threshold(gray, 180, 255, type=cv.THRESH_TOZERO)
+
+cv.imshow("2.1 - treshold tozero", thresholdImage)
+
+e2 = cv.getTickCount()
+t = (e2 - e1)/cv.getTickFrequency()
+print( "tozero  : " ,t)
+
 ##noir & blanc
-(thresh, blackAndWhiteImage) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+e1 = cv.getTickCount()
+(thresh, blackAndWhiteImage) = cv.threshold(thresholdImage, 127, 255, cv.THRESH_BINARY)
 
 e2 = cv.getTickCount()
 t = (e2 - e1)/cv.getTickFrequency()
@@ -72,8 +108,19 @@ t = (e2 - e1)/cv.getTickFrequency()
 print( "erosion  : ", t )
 
 cv.imshow("2.2 - erosion", erosion)
-k = cv.waitKey(0)
 
+
+"""
+cv.namedWindow(window_name)
+cv.createTrackbar(trackbar_type, window_name , 3, max_type, Threshold_Demo)
+# Create Trackbar to choose Threshold value
+cv.createTrackbar(trackbar_value, window_name , 0, max_value, Threshold_Demo)
+# Call the function to initialize
+Threshold_Demo(0)
+# Wait until user finishes program
+cv.waitKey()
+k = cv.waitKey(0)
+"""
 
 """
 ##threshold avec moyenne

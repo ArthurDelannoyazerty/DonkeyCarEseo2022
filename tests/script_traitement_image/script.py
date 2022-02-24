227e91dcac3c60433cf262e493b217c62e3984ca
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2022-02-24 15:03:40 +0100

    test treshold tozero birghtness

--- tests/script_traitement_image/script.py
+++ tests/script_traitement_image/script.py
@@ -1,7 +1,6 @@
-import argparse
+#import argparse
 import cv2 as cv
 import sys
-from cv2 import threshold
 import numpy as np
 
 """
@@ -24,12 +23,12 @@ def Threshold_Demo(val):
     cv.imshow(window_name, dst)
 
 parser = argparse.ArgumentParser(description='Code for Basic Thresholding Operations tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='867_cam_image_array_.jpg')
+parser.add_argument('--input', help='Path to input image.', default='original_2k22.jpg')
 args = parser.parse_args()
 """
 
 ##load imagesv
-origin = cv.imread(cv.samples.findFile("867_cam_image_array_.jpg"))
+origin = cv.imread(cv.samples.findFile("original_2k22.jpg"))
 if origin is None:
     sys.exit("Could not read the image.")
 
@@ -49,40 +48,55 @@ crop_img = origin[y:y+h, x:x+w]
 cv.imshow("0.1 - masque numpy", crop_img)
 
 ##ajout masque
+"""
 e1 = cv.getTickCount()
 
-#wMask = cv.bitwise_and(origin,mask)
-
+wMask = cv.bitwise_and(origin,mask)
 
 e2 = cv.getTickCount()
 t = (e2 - e1)/cv.getTickFrequency()
 print( "ajout mask  : ",t)
 
 #cv.imshow("1 - mask", wMask)
-
+"""
 ##niveau de gris
 e1 = cv.getTickCount()
 
 gray = cv.cvtColor(crop_img, cv.COLOR_BGR2GRAY)
-cv.imshow("2 - gray", gray)
 
 e2 = cv.getTickCount()
+
+cv.imshow("2 - gray", gray)
+
 t = (e2 - e1)/cv.getTickFrequency()
 print( "gray  : " ,t)
 
+#blur
+"""
+e1 = cv.getTickCount()
+i=5
+blured = cv.GaussianBlur(gray, (i, i), 0)
+
+t = (e2 - e1)/cv.getTickFrequency()
+print( "blur  : " ,t)
+
+cv.imshow("2.0.5 - bilateral filter", blured)
+"""
 
 #threshold
 e1 = cv.getTickCount()
 
-_,thresholdImage = cv.threshold(gray, 180, 255, type=cv.THRESH_TOZERO)
+_,thresholdImage = cv.threshold(gray, 190, 255, type=cv.THRESH_TOZERO)
+
+e2 = cv.getTickCount()
 
 cv.imshow("2.1 - treshold tozero", thresholdImage)
 
-e2 = cv.getTickCount()
 t = (e2 - e1)/cv.getTickFrequency()
 print( "tozero  : " ,t)
 
 ##noir & blanc
+"""
 e1 = cv.getTickCount()
 (thresh, blackAndWhiteImage) = cv.threshold(thresholdImage, 127, 255, cv.THRESH_BINARY)
 
@@ -91,16 +105,16 @@ t = (e2 - e1)/cv.getTickFrequency()
 print( "black & white  : ",t)
 
 cv.imshow("2.1 - noir/blanc", blackAndWhiteImage)
-
+"""
 ##erosion
 e1 = cv.getTickCount()
 
-kernel_size = 3     #3,5,7 aua choix
-#kernel = np.ones((kernel_size,kernel_size),np.uint8)
+kernel_size = 2     #3,5,7 aua choix
+kernel = np.ones((kernel_size,kernel_size),np.uint8)
 #kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(kernel_size,kernel_size))
-kernel = cv.getStructuringElement(cv.MORPH_CROSS,(kernel_size,kernel_size))
+#kernel = cv.getStructuringElement(cv.MORPH_CROSS,(kernel_size,kernel_size))
 
-erosion = cv.erode(blackAndWhiteImage,kernel,iterations = 1)
+erosion = cv.erode(thresholdImage,kernel,iterations = 1)
 #erosion = cv.erode(blackAndWhiteImage,cv.MORPH_OPEN,kernel)
 
 e2 = cv.getTickCount()
@@ -108,7 +122,7 @@ t = (e2 - e1)/cv.getTickFrequency()
 print( "erosion  : ", t )
 
 cv.imshow("2.2 - erosion", erosion)
-
+cv.waitKey()
 
 """
 cv.namedWindow(window_name)
@@ -119,7 +133,6 @@ cv.createTrackbar(trackbar_value, window_name , 0, max_value, Threshold_Demo)
 Threshold_Demo(0)
 # Wait until user finishes program
 cv.waitKey()
-k = cv.waitKey(0)
 """
 
 """
